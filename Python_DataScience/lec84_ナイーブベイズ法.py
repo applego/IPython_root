f617d9518ce0d7b5c346e96040af4865fcae1580
Tsuneshige Yusuke <tokoshige_mac@TsuneshigenoMacBook-Pro.local>
2019-04-02 01:46:54 +0900

    多クラス分類、SVM

--- Python_DataScience/lec84_ナイーブベイズ法.py
+++ Python_DataScience/lec84_ナイーブベイズ法.py
@@ -6,4 +6,10 @@
 #       format_name: light
 #       format_version: '1.3'
 #       jupytext_version: 1.0.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
+
+
